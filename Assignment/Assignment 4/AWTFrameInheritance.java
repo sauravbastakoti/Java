50d85e04e5ce983495f0d9d616a5c723bae79bf9
Saurav Bastakoti <81146325+sauravbastakoti@users.noreply.github.com>
2022-01-11 21:06:10 +0545

    Add files via upload

---
+++ Assignment/Assignment 4/AWTFrameInheritance.java
@@ -0,0 +1,51 @@
+import java.awt.Button;
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.*;  
+
+class AWTFrameInheritance extends Frame // Inherating Frame 
+{
+	public static void main(String[] args) {
+		AWTFrameInheritance frame = new AWTFrameInheritance();
+		Button a=new Button("Add");
+		Button b=new Button("Subract");
+		Button c=new Button("Multiply");
+		Button d=new Button("Divide");
+		TextField t1 = new TextField();   
+        TextField t2 = new TextField();   
+        TextField t3 = new TextField();
+        Label l1 = new Label("Number 1 :");
+        Label l2 = new Label("Number 2 :");
+        Label l3 = new Label("Result :");
+
+        t1.setBounds(150, 100, 250, 30);  
+        t2.setBounds(150, 150, 250, 30);   
+        t3.setBounds(150, 200, 250, 30);     
+
+
+		a.setBounds(30,300,95,50); 
+		b.setBounds(140,300,95,50); 
+		c.setBounds(250,300,95,50); 
+		d.setBounds(360,300,95,50); 
+		l1.setBounds(30, 100, 100, 30);    
+        l2.setBounds(30, 150, 100, 30);  
+        l3.setBounds(30, 200, 100, 30);  
+
+
+        
+		frame.setSize(500, 400);
+		frame.add(l1);
+		frame.add(l2);
+		frame.add(l3);
+		frame.add(t1);
+		frame.add(t2);
+		frame.add(t3);
+		frame.add(a);
+		frame.add(b);
+		frame.add(c);
+		frame.add(d);
+
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+}
---
+++ Assignment/Assignment 4/SWINGFrameInheritance.java
@@ -0,0 +1,51 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.*;
+
+class SWINGFrameInheritance extends JFrame
+{
+	public static void main(String[] args) {
+		
+		SWINGFrameAssociation frame = new SWINGFrameAssociation();
+		JButton a=new JButton("Add");
+		JButton b=new JButton("Subract");
+		JButton c=new JButton("Multiply");
+		JButton d=new JButton("Divide");
+		JTextField t1 = new JTextField();   
+        JTextField t2 = new JTextField();   
+        JTextField t3 = new JTextField();
+        JLabel l1 = new JLabel("Number 1 :");
+        JLabel l2 = new JLabel("Number 2 :");
+        JLabel l3 = new JLabel("Result :");
+
+        t1.setBounds(150, 100, 250, 30);  
+        t2.setBounds(150, 150, 250, 30);   
+        t3.setBounds(150, 200, 250, 30);     
+
+
+		a.setBounds(30,300,95,50); 
+		b.setBounds(140,300,95,50); 
+		c.setBounds(250,300,95,50); 
+		d.setBounds(360,300,95,50); 
+		l1.setBounds(30, 100, 100, 30);    
+        l2.setBounds(30, 150, 100, 30);  
+        l3.setBounds(30, 200, 100, 30);  
+
+
+        
+		frame.setSize(500, 400);
+		frame.add(l1);
+		frame.add(l2);
+		frame.add(l3);
+		frame.add(t1);
+		frame.add(t2);
+		frame.add(t3);
+		frame.add(a);
+		frame.add(b);
+		frame.add(c);
+		frame.add(d);
+
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+}
